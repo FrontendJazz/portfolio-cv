693ebaff44786273f3367fb456652ff2ba26a20b
Samartsev Igor <igorsamartsev@igorsamartsev.ru>
2021-06-15 01:03:00 +0600

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+// ==========   Menu show & hide
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
+
+// =========  Show menu and validate
+
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+// =========  Hide menu and validate
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+// ======== Remove when click link item
+
+const navLinks = document.querySelectorAll('.nav__link')
+const linkActions = () =>  navMenu.classList.remove('show-menu')
+navLinks.forEach(el => el.addEventListener('click', linkActions))
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// SKILL ACCORDION
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+  skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className
+  for (let i =0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'
+  }
+  if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+skillsHeader.forEach(el => {
+  el.addEventListener('click', toggleSkills)
+})
