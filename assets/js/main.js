ee493b8c3a4353034fcf98b4f429f202de63c9e8
Samartsev Igor <igorsamartsev@igorsamartsev.ru>
2021-06-15 10:06:37 +0600

    qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,22 @@ function toggleSkills() {
 skillsHeader.forEach(el => {
   el.addEventListener('click', toggleSkills)
 })
+
+// Qualification tabs
+
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+    tabs.forEach(el => {
+      el.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+})
